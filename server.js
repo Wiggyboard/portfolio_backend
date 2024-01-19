c786a62c972081d2352a73c5490e25977851192c
Wiggyboard <105466565+Wiggyboard@users.noreply.github.com>
2024-01-18 20:09:18 -0800

    Updated body parser

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/send', (req, res) => {
     console.log(req.body);
